822e639c025251acb06937c4899b7ae46a404fc2
Chananan Likitsarawit <chananan.anu@mail.kmutt.ac.th>
2021-03-07 12:55:54 +0700

    [Finished]

--- profile.js
+++ profile.js
@@ -1,16 +1,61 @@
-const app = {
-    data() {
-        return {
-            firstname: 'Chananan',
-            lastname: 'Likitsarawit',
-            id: '62130500111',
-            university: 'KMUTT',
-            faculty: 'School of Infomation Technology',
-            major: 'Information Technology',
-            city: 'Bangkok Thailand',
-            image: './images/image.jpg',
-        }
+const constraints = {
+    firstname: {
+        presence: true,
+    },
+    lastname: {
+        presence: true,
+    },
+    id: {
+        presence: true,
+    },
+    gender: {
+        presence: true,
+    },
+    email: {
+        presence: true,
+        email: true
+    },
+    phone: {
+        presence: true,
+        length: {
+            minimum: 10,
+            message: "must be at least 10 digits"
+        },
     }
 }
 
-mountApp = Vue.createApp(app).mount('#app')
+const app = Vue.createApp({
+    data() {
+        return {
+            image: './profile.png',
+            firstname: null,
+            lastname: null,
+            id: null,
+            gender: null,
+            email: null,
+            phone: null,
+            errors: null,
+        }
+    },
+
+    methods: {
+        checkForm() {
+            this.errors = validate({
+                firstname: this.firstname,
+                lastname: this.lastname,
+                id: this.id,
+                gender: this.gender,
+                email: this.email,
+                phone: this.phone,
+            },constraints);
+
+            if(this.errors){
+                console.log(this.errors);
+            } else {
+                alert("Your profile is updated successfully.")
+            }
+        },
+    }
+})
+
+app.mount('#app')
